4e4d67bc2698acd4811a1a26f96dc96de2439660
vermad843 <vishnuverma7524@gmail.com>
2020-11-05 20:56:54 +0530

    map

--- example.js
+++ example.js
@@ -232,52 +232,168 @@
 
 //  forloops : 
 
-let things = ['shoe', 'table', 'book', 'computer', 'phone'];
-let copiedThings = [];
+// let things = ['shoe', 'table', 'book', 'computer', 'phone'];
+// let copiedThings = [];
 
-for (let i = 0; i < things.length; i++) {
-   const element = things[i];
-   copiedThings.push(element);
-}
+// for (let i = 0; i < things.length; i++) {
+//    const element = things[i];
+//    copiedThings.push(element);
+// }
 
-console.log(copiedThings);
+// console.log(copiedThings);
 
 
-// forEach 
+// // forEach 
 
-things.forEach((number) => {
-   copiedThings.push(number);
+// things.forEach((number) => {
+//    copiedThings.push(number);
+// })
+
+// console.log(copiedThings);
+
+
+
+// // forloops : 
+
+
+// let all_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let evenNumbers = [];
+
+// for (let i = 0; i < all_numbers.length; i++) {
+//    const element = all_numbers[i];
+//    if(element % 2 === 0) {
+//       evenNumbers.push(element);
+//    } 
+// }
+
+// console.log(evenNumbers);
+
+
+// // forEach 
+
+// all_numbers.forEach((number) => {
+//    if(number % 2 === 0) {
+//       evenNumbers.push(number);
+//    }
+// });
+
+// console.log(evenNumbers);
+
+
+
+// 
+// map : 
+
+let numbers = [1, 2, 3];
+
+let doubledNumbers = numbers.map((number) => {
+   return number*2;
 })
 
-console.log(copiedThings);
+console.log(doubledNumbers);
+
+
+// 
 
 
 
-// forloops : 
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+const threeLetters = days.map((day) => {
+   // return `${day[0]}${day[1]}${day[2]}`;
+   return day.substring(0,3);
+})
+
+console.log(threeLetters);
 
 
-let all_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let evenNumbers = [];
+// 
 
-for (let i = 0; i < all_numbers.length; i++) {
-   const element = all_numbers[i];
-   if(element % 2 === 0) {
-      evenNumbers.push(element);
-   } 
-}
+const people = [{
+   first_name : 'Dk',
+   last_name : 'verma'
+},{
+   first_name : 'vishnu',
+   last_name : 'verma'
+}, {
+   first_name : 'hey',
+   last_name : 'man'
+}, {
+   first_name : 'kd',
+   last_name : 'uhho'
+}];
 
-console.log(evenNumbers);
+
+const fullName = people.map((person) => {
+   return `${person.first_name} ${person.last_name}`;
+}) 
+
+console.log(fullName);
 
 
-// forEach 
 
-all_numbers.forEach((number) => {
-   if(number % 2 === 0) {
-      evenNumbers.push(number);
+// 
+
+const animals = [{
+   name : 'cat',
+   size : 'small'
+}, {
+   name : 'dog',
+   size : 'small'
+}, {
+   name : 'lion',
+   size : 'medium'
+}, {
+   name : 'elephant',
+   size : 'big' 
+}];
+
+const animalName = animals.map((animal) => {
+   return `${animal.name}`
+});
+
+console.log(animalName);
+
+
+// 
+
+const theNumbers = [4, 8, 15, 16, 23, 42];
+
+const valueAndIndex = theNumbers.map((number, index) => {
+   return {
+      number,
+      index
    }
 });
 
-console.log(evenNumbers);
+console.log(valueAndIndex);
+
+
+// 
+
+
+const starter = [2, 5, 10];
+
+const multiplied = starter.map((start, index, arr) => {
+   let nextIndex = index + 1;
+   if(index == arr.length-1) {
+      nextIndex = 0;
+   }
+   return start*arr[nextIndex];
+})
+
+console.log(multiplied);
+
+
+
 
 
 
